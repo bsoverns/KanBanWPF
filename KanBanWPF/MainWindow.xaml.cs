6fac549daf2d031c1c3fe2de1fa62d1abc08d795
Brad <bsoverns@gmail.com>
2024-04-06 12:52:38 -0400

    First push

---
+++ KanBanWPF/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KanBanWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Create a new card and add it to the column "Not Started"
+        private void AddCardButton_Click(object sender, RoutedEventArgs e)
+        {
+            KanbanCard card = new KanbanCard();
+            card.Title = "New Task";
+            card.Description = "Description of the task";
+            NotStartedColumn.Children.Add(card);
+        }
+
+
+    }
+}
